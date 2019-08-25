5110e1ff296b2367bdd01905346385e044bb7242
Amanda Withrow <amandawithrow@Amandas-MacBook-Pro.local>
2019-08-25 15:45:39 -0700

    added greatest increase in profits

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,10 @@ months = 0
 #declare variable for total profit/loss
 net_total = 0
 
+#declare lists to find greatest increase and decrease later
+calendar = []
+profit = []
+
 # Read in the CSV file
 with open(bank_csv, 'r') as csvfile:
     
@@ -21,12 +25,34 @@ with open(bank_csv, 'r') as csvfile:
 
     #Loop through data to count rows and get net total profit/loss
     for row in csvreader:
+        
         months += 1
         month_total = float(row[1])
         net_total = net_total + month_total
+        
+
 
 #calculate net average profit/loss
 avg_profit = round(net_total/months, 2)
+
+with open(bank_csv, 'r') as csvfile:
+    
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    #skip header row
+    header = next(csvreader)
+
+    #Loop through data to count rows and get net total profit/loss
+    for row in csvreader:
+        calendar.append(row[0])
+        profit.append(int(row[1]))
+
+#Find Greatest increase in profits
+maxpos = profit.index(max(profit))
+money = max(profit)
+
+
 
 #print summary
 print("Finanacial Analysis")
@@ -34,6 +60,7 @@ print("---------------------------------")
 print(f"Total Months: {months}")
 print(f"Total: {net_total}")
 print(f"Average Change: {avg_profit}")
+print(f"Greatest Increase in Profits: {calendar[maxpos]}: {profit[maxpos]}")
 
 
 
