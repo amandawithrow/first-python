276df6626ad3c23b2645152110856c0de5da23bb
Amanda Withrow <amandawithrow@Amandas-MacBook-Pro.local>
2019-08-31 13:12:59 -0700

    added last elements of PyParagrah.  All code works on test file and gatsby example

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,21 +1,23 @@
+import re
+
 #open file with paragraph to read it
-gatsby = open("gatsby paragraph.txt", "r")
+doc = open("test.txt", "r")
 
 #read whole text file
-whole = gatsby.read()
+paragraph = doc.read()
 
 #close file
-gatsby.close()
+doc.close()
 
-#find total word count
-words = whole.split(" ")
+#find total word count by splitting at spaces between words
+words = paragraph.split(" ")
 total_word_count = len(words)
 
-#find total sentence count
-sentence = whole.split(".")
+#find total sentence count by splitting at periods
+sentence = paragraph.split(".")
 total_sentence = len(sentence) - 1
 
-#find letter count per word
+#find letter count per word by iterating through each word in list and counting
 total_letters = 0
 i = 0
 while i < total_word_count:
@@ -25,11 +27,23 @@ while i < total_word_count:
 
 letter_count = round(total_letters/total_word_count, 2)
 
+sentence_list = re.split("(?<=[.!?]) +", paragraph)
+j=0
+sentence_length = 0
+
+while j < total_sentence:
+    wordy = re.findall("[a-zA-Z_]+", sentence_list[j])
+    sentence_length = sentence_length + len(wordy)
+    j+=1
+
+avg_sentence = round((sentence_length/total_sentence), 2)
+
 print("Paragraph Analysis")
 print("-------------------")
 print(f"Approximate word count: {total_word_count}")
 print(f"Approximate sentence count: {total_sentence}")
 print(f"Average Letter count: {letter_count}")
+print(f"Average Sentence Length: {avg_sentence}")
 
 
 
